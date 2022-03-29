c0380769b90c0542029d3e17fd0ecde3817b3358
calvin <cal.villa@icloud.com>
2022-03-28 20:45:25 -0400

    set up more files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,27 @@
+
+const Employee = require ('../lib/Employee');
+
+//name,email,id,role validation
+
+const employeeTest = new Employee ("Calvin", "cal.villa@icloud.com", 3 , "Employee");
+
+
+describe('Employee', () => {
+    it('has a name', () => {
+        expect(employeeTest.name).toEqual(expect.any(String))
+        expect(employeeTest.name.length).toBeGreaterThan(2)
+    })
+
+    it('has an email id', () =>{
+        expect(employeeTest.email).toEqual(expect.stringContaining('@'))
+    })
+
+    it('has an id that is a num', () => {
+        expect(employeeTest.id).toEqual(expect.any(Number))
+    })
+
+    it('has a role of employee', () => {
+        expect(employeeTest.role).toBe('Employee')
+    })
+
+})
